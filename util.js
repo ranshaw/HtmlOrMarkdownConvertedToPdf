078f7e600ba3fd4e450783aab3fd84c0e4ab8bf8
tangyunxiao <tangyx@lybrc.com.cn>
2018-11-19 11:31:01 +0800

    :sparkles: feat:删除无用代码

--- util.js
+++ util.js
@@ -12,39 +12,6 @@ function parseBody(url) {
   });
 }
 
-const htmlToPdf = async pdfUrls => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-
-  var pdfFiles = [];
-
-  for (var i = 0; i < pdfUrls.length; i++) {
-    await page.goto(pdfUrls[i], { waitUntil: "networkidle0" });
-
-    var pdfFileName = "sample" + (i + 1) + ".pdf";
-    pdfFiles.push(pdfFileName);
-    await page.pdf({ path: pdfFileName, format: "A6" });
-  }
-
-  await browser.close();
-
-  await _mergeMultiplePDF(pdfFiles);
-};
-
-const _mergeMultiplePDF = pdfFiles => {
-  return new Promise((resolve, reject) => {
-    merge(pdfFiles, "samplefinal.pdf", function(err) {
-      if (err) {
-        console.log(err);
-        reject(err);
-      }
-
-      console.log("Success");
-      resolve();
-    });
-  });
-};
-
 module.exports = {
   parseBody
 };
